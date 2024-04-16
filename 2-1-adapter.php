edd2e5c87c4e08204bb7f99bcf5e18a23ed7eab6
Billy VILLENA <coding@devrendal.fr>
2024-04-16 15:39:12 +0200

    ✨ [Structure] Adapter
    > Adapter sur l'export d'une liste d'événements (entité) en tant que donnée CSV et JSON.

---
+++ 2-1-adapter.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+use EsgiIw\TpDesignPattern\Adapter\Entity\Event\Export\EventCsvExportAdapter;
+use EsgiIw\TpDesignPattern\Adapter\Entity\Event\Export\EventJsonExportAdapter;
+use EsgiIw\TpDesignPattern\Model\Entity\Event;
+
+require_once 'vendor/autoload.php';
+
+// Récupération des événements
+$eventsToExport = [
+    (new Event())
+        ->setId(1)
+        ->setLabel('Gala du printemps')
+        ->setDescription('Assistez à la soirée de gala pour le printemps.')
+        ->setDate(new \DateTime('2024-04-26 19:30')),
+
+    (new Event())
+        ->setId(2)
+        ->setLabel('Soirée League of Legend')
+        ->setDescription('Rejoignez vos comparses sur LoL.')
+        ->setDate(new \DateTime('2024-04-20 21:00')),
+
+    (new Event())
+        ->setId(5)
+        ->setLabel('Afterwork de fin avril')
+        ->setDescription('Tous au bar pour la fin du mois d\'avril.')
+        ->setDate(new \DateTime('2024-04-30 18:00')),
+];
+
+// Définition des adapteurs
+$adapters = [
+    new EventCsvExportAdapter($eventsToExport),
+    new EventJsonExportAdapter($eventsToExport),
+];
+
+// Utilisation des adapteurs
+foreach ($adapters as $adapter) {
+    echo '# Adapter ' . $adapter::class . "\n";
+    echo $adapter->convert();
+    echo "\n\n";
+}
